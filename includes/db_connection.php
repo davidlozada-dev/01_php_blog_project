2de5b4657336793b454492a441a291a5384e7d87
David <davidlozada.developer@gmail.com>
2021-04-25 02:21:24 -0400

    Update db_connection file
    Update db_connection file

---
+++ includes/db_connection.php
@@ -0,0 +1,12 @@
+<?php 
+//database connection
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "01_php_blog_project";
+
+
+$db = mysqli_connect($server, $username, $password, $database);
+
+?>
